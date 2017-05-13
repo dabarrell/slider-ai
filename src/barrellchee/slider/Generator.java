4590868486b8d36d9d6c20d1b91282f6b637e182
David Barrell <david@barrell.me>
2017-05-14 03:06:10 +1000

    Created game data generator

---
+++ src/barrellchee/slider/Generator.java
@@ -0,0 +1,26 @@
+package barrellchee.slider;
+
+import aiproj.slider.Referee;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * ai-partB
+ * Created by David Barrell on 14/5/17.
+ */
+public class Generator {
+    private static int count = 1;
+
+    public static void main(String[] args) {
+        while (count < 10000) {
+            int dimension = ThreadLocalRandom.current().nextInt(5, 7 + 1);
+            System.out.println("Starting game number " + count + " with dimension " + dimension);
+            Referee.main(new String[]{
+                    String.valueOf(dimension)
+                    , "barrellchee.slider.RandomPlayer"
+                    , "barrellchee.slider.RandomPlayer"
+            });
+            count++;
+        }
+    }
+}
